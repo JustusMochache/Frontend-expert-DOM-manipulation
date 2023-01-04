a8e25d4b383b6e18ba54bad8f7d5cb240405ef9b
Justus <justus.onyancha@moringaschool.com>
2023-01-04 04:17:16 +0300

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Tester");
+
+const secli = document.getElementById('second-li');
+const firstli = document.querySelector('li');
+console.log(secli);
+
+const allList = document.querySelectorAll('li')
+
+console.log(allList.forEach((li) => {
+    console.log(li);
+}));
